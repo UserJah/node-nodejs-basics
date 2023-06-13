39bb180da128485cf0c786ac14e4623a6606b1c2
Rustam <dzhaksimov.rustam@gmail.com>
2023-06-13 23:41:22 +0300

    refactor: prettier

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,21 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const create = async () => {
-    const filePath = 'files/fresh.txt';
-    const fileData = 'I am fresh and young';
-    const fileExistError = 'FS operation failed';
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename)
-
-    fs.access(path.join(__dirname, filePath), (err, data) => {
-        if (err && !data) {
-            fs.writeFile(path.join(__dirname, filePath), fileData, () => {});
-        } else {
-            throw Error(fileExistError);
-        }
-    })
+  const filePath = "files/fresh.txt";
+  const fileData = "I am fresh and young";
+  const fileExistError = "FS operation failed";
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  
+  fs.access(path.join(__dirname, filePath), (err, data) => {
+    if (err && !data) {
+      fs.writeFile(path.join(__dirname, filePath), fileData, () => {});
+    } else {
+      throw Error(fileExistError);
+    }
+  });
 };
 
 await create();
